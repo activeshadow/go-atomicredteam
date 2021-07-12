59bff55d8040d1ebf0d0b87d86859c51af75a0a9
Bryan T. Richardson <bryan@activeshadow.com>
2021-07-12 13:31:35 -0600

    Update download-atomics script to include a "force" option

--- download-atomics.sh
+++ download-atomics.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
 
-repo=redcanaryco/master
+usage="usage: $(basename "$0") [-d] [-h] [-v]
 
-if [[ -n $1 ]]; then
-  repo=$1
-fi
+Download latest version of atomic-red-team atomic tests from the given
+repository owner and branch (defaults to redcanaryco/master). This script
+assumes the repository name is 'atomic-red-team'.
+
+This script will exit early if the include/atomics directory already exists
+unless the 'force' option is provided.
+
+where:
+    -f      force download of latest atomics
+    -h      show this help text
+    -r      repo owner and branch to download from"
+
+
+repo=redcanaryco/master
+force=false
+
+
+# loop through positional options/arguments
+while getopts ':fhr:' option; do
+    case "$option" in
+        f)  force=true             ;;
+        h)  echo -e "$usage"; exit ;;
+        r)  repo="$OPTARG"         ;;
+        \?) echo -e "illegal option: -$OPTARG\n" >$2
+            echo -e "$usage" >&2
+            exit 1 ;;
+    esac
+done
 
 IFS='/' read -ra repo <<< "$repo"
 
@@ -13,7 +38,7 @@ if (( ${#repo[@]} != 2 )); then
   exit 1
 fi
 
-[ -d include/atomics ] && echo "\
+[ -d include/atomics ] && [ "$force" = false ] && echo "\
   Atomics already exist - not overwriting.
   Delete include/atomics directory and rerun if you want to reinstall." \
   && exit 0
@@ -26,7 +51,8 @@ curl -L -o art.zip $url
 
 echo "Unarchiving repo"
 
-unzip art.zip && rm art.zip
+unzip art.zip
+rm art.zip
 
 echo "Copying archived atomics to 'include' directory"
 
